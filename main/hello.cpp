88843a51b1cf1677755a20eaebde1e36925ccc68
Vishal7CE <vishalconjeevaram@gmail.com>
2023-02-19 00:22:22 +0530

    This commit is after making a change in the cpp file for task 4

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,7 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "This is Vishal C E. My SRN is PES2UG20CS386.\n";
-  cout << "This line is the change I have made in this cpp file for task 4.\n";
+  cout << "This is the change I have made in this cpp file for Task 4.\n";
   cout << "I have successfully built and run\n";
   return 0;
 }
